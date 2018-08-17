47501eb993f78a3e57991b1fad279503fe07825c
Mack Stanley <mlgprohacker@gmail.com>
2018-08-16 19:45:20 -0500

    do eclipse test

---
+++ eclipseTest/foobar/MainClass.java
@@ -0,0 +1,12 @@
+package foobar;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// We're here today to test out Git functionality in Eclipse.
+		
+		System.out.println("HELLO, SOCIAL CODING PLATOFRM OF CHOICE.");
+
+	}
+
+}
